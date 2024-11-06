1c41c7bd75828d1cca0a2b8de25700e6f7b3bc0b
riabu <riabumary@gmail.com>
2024-11-06 15:07:16 +0300

    New project

---
+++ login_register/main.php
@@ -0,0 +1,50 @@
+<!-- Banner -->
+<div>
+			<section class="banner full">
+				<article>
+					<img src="img/slider/slide01.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Sure we have millions of prints and Sculptures from all variety.</p>
+							<h2>OUR GUARANTEE</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide02.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>we also have over 200 frame styles, plus canvas, wood Curvings, and art on metal & Wood.</p>
+							<h2>CUSTOM FINISHED</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide03.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>If you don’t love it, return it. It’s just that easy.</p>
+							<h2>OUR GUARANTEE</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide04.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Welcome to our online shop make your orders today for beautiful Artwork and Sculptures.</p>
+							<h2>Classic Art Modern Way</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide05.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Visual search helps you find the perfect piece aligning with your Taste</p>
+							<h2>Find Art with Visual Search</h2>
+						</header>
+					</div>
+				</article>
+			</section>
+			</div>
